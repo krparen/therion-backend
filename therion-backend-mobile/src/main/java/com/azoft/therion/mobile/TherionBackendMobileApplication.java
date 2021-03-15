da190587d82db85f821497043aecc23f73c1a85b
g.eremeev <g.eremeev@azoft.com>
2021-03-15 08:55:54 +0300

    mobile module stub

---
+++ therion-backend-mobile/src/main/java/com/azoft/therion/mobile/TherionBackendMobileApplication.java
@@ -0,0 +1,13 @@
+package com.azoft.therion.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TherionBackendMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TherionBackendMobileApplication.class, args);
+	}
+
+}
---
+++ therion-backend-mobile/src/test/java/com/azoft/therion/mobile/TherionBackendMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azoft.therion.mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TherionBackendMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
